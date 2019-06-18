305fef0ace76083fb0a7495e6b9b0ecfe5ca34a2
Nicolas Belan <>
2019-06-18 11:09:02 +0200

    fix comment

--- client.php
+++ client.php
@@ -66,7 +66,11 @@ if (isset($_REQUEST['logout'])) {
   </head>
   <body>
     <h1>Successfull Authentication!</h1>
-    <?php require 'script_info.php'?>
+    <dl style='border: 1px dotted; padding: 5px;'>
+        <dt>Current script</dt><dd><?php print basename($_SERVER['SCRIPT_NAME']); ?></dd>
+        <dt>session_name():</dt><dd> <?php print session_name(); ?></dd>
+        <dt>session_id():</dt><dd> <?php print session_id(); ?></dd>
+    </dl>
     <p>the user's login is <b><?php echo phpCAS::getUser();?></b>.</p>
     <p>phpCAS version is <b><?php echo phpCAS::getVersion();?></b>.</p>
 <h3>User Attributes</h3>
